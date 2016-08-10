cdec1188ab58aca95ff03732dfbe180da8366256
jimmyfingers <Home@Jamess-MacBook-Pro.local>
2016-08-10 17:51:41 +0100

    Updated about page copy

--- public/bundle.js
+++ public/bundle.js
@@ -26375,9 +26375,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to the about page.'
+	    )
 	  );
 	};
 
